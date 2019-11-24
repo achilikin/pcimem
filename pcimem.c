54ad0059b706160ec41077b339b55afbe429286b
Andrey Chilikin <achilikin@gmail.com>
2019-11-24 21:08:11 +0000

    Fix formatting and add verbose mode
    For verbose mode use pcimemv link to pcimem

--- pcimem.c
+++ pcimem.c
@@ -15,7 +15,7 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
@@ -27,6 +27,7 @@
 #include <stdint.h>
 #include <unistd.h>
 #include <string.h>
+#include <libgen.h>
 #include <errno.h>
 #include <signal.h>
 #include <fcntl.h>
@@ -41,8 +42,8 @@
 		__LINE__, __FILE__, errno, strerror(errno)); exit(1); \
 	} while(0)
 
-
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
 	int fd;
 	void *map_base, *virt_addr;
 	uint64_t read_result, writeval, prev_read_result = 0;
@@ -56,7 +57,7 @@ int main(int argc, char **argv) {
 	int i;
 	int map_size = 4096UL;
 
-	if(argc < 3) {
+	if (argc < 3) {
 		// pcimem /sys/bus/pci/devices/0001\:00\:07.0/resource0 0x100 w 0x00
 		// argv[0]  [1]                                         [2]   [3] [4]
 		fprintf(stderr, "\nUsage:\t%s { sysfile } { offset } [ type*count [ data ] ]\n"
@@ -68,88 +69,94 @@ int main(int argc, char **argv) {
 			argv[0]);
 		exit(1);
 	}
+
+	if (strcmp(basename(argv[0]), "pcimemv") == 0)
+		verbose = 1;
 	filename = argv[1];
 	target = strtoul(argv[2], 0, 0);
 
-	if(argc > 3) {
+	if (argc > 3) {
 		access_type = tolower(argv[3][0]);
 		if (argv[3][1] == '*')
 			items_count = strtoul(argv[3]+2, 0, 0);
 	}
 
-        switch(access_type) {
-		case 'b':
-			type_width = 1;
-			break;
-		case 'h':
-			type_width = 2;
-			break;
-		case 'w':
-			type_width = 4;
-			break;
-                case 'd':
-			type_width = 8;
-			break;
-		default:
-			fprintf(stderr, "Illegal data type '%c'.\n", access_type);
-			exit(2);
+	switch (access_type) {
+	case 'b':
+		type_width = 1;
+		break;
+	case 'h':
+		type_width = 2;
+		break;
+	case 'w':
+		type_width = 4;
+		break;
+	case 'd':
+		type_width = 8;
+		break;
+	default:
+		fprintf(stderr, "Illegal data type '%c'.\n", access_type);
+		exit(2);
 	}
 
-    if((fd = open(filename, O_RDWR | O_SYNC)) == -1) PRINT_ERROR;
-    printf("%s opened.\n", filename);
-    printf("Target offset is 0x%x, page size is %ld\n", (int) target, sysconf(_SC_PAGE_SIZE));
-    fflush(stdout);
+    if ((fd = open(filename, O_RDWR | O_SYNC)) == -1) PRINT_ERROR;
+	if (verbose) {
+		printf("%s opened.\n", filename);
+		printf("Target offset is 0x%x, page size is %ld\n", (int)target, sysconf(_SC_PAGE_SIZE));
+		fflush(stdout);
+	}
 
     target_base = target & ~(sysconf(_SC_PAGE_SIZE)-1);
     if (target + items_count*type_width - target_base > map_size)
 	map_size = target + items_count*type_width - target_base;
 
     /* Map one page */
-    printf("mmap(%d, %d, 0x%x, 0x%x, %d, 0x%x)\n", 0, map_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, (int) target);
+	if (verbose)
+		printf("mmap(%d, %d, 0x%x, 0x%x, %d, 0x%x)\n", 0, map_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, (int) target);
 
     map_base = mmap(0, map_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, target_base);
-    if(map_base == (void *) -1) PRINT_ERROR;
-    printf("PCI Memory mapped to address 0x%08lx.\n", (unsigned long) map_base);
-    fflush(stdout);
-
-    for (i = 0; i < items_count; i++) {
-
-        virt_addr = map_base + target + i*type_width - target_base;
-        switch(access_type) {
-		case 'b':
-			read_result = *((uint8_t *) virt_addr);
-			break;
-		case 'h':
-			read_result = *((uint16_t *) virt_addr);
-			break;
-		case 'w':
-			read_result = *((uint32_t *) virt_addr);
-			break;
-                case 'd':
-			read_result = *((uint64_t *) virt_addr);
-			break;
+    if (map_base == (void *) -1) PRINT_ERROR;
+	if (verbose) {
+		printf("PCI Memory mapped to address 0x%08lx.\n", (unsigned long)map_base);
+		fflush(stdout);
 	}
 
-    	if (verbose)
-            printf("Value at offset 0x%X (%p): 0x%0*lX\n", (int) target + i*type_width, virt_addr, type_width*2, read_result);
-        else {
-	    if (read_result != prev_read_result || i == 0) {
-                printf("0x%04X: 0x%0*lX\n", (int)(target + i*type_width), type_width*2, read_result);
-                read_result_dupped = 0;
-            } else {
-                if (!read_result_dupped)
-                    printf("...\n");
-                read_result_dupped = 1;
-            }
-        }
-	
-	prev_read_result = read_result;
+    for (i = 0; i < items_count; i++) {
+        virt_addr = map_base + target + i*type_width - target_base;
+		switch (access_type) {
+		case 'b':
+			read_result = *((uint8_t *)virt_addr);
+			break;
+		case 'h':
+			read_result = *((uint16_t *)virt_addr);
+			break;
+		case 'w':
+			read_result = *((uint32_t *)virt_addr);
+			break;
+		case 'd':
+			read_result = *((uint64_t *)virt_addr);
+			break;
+		}
 
+		if (verbose)
+			printf("Value at offset 0x%X (%p): 0x%0*lX\n", (int)target + i*type_width, virt_addr, type_width * 2, read_result);
+		else if (argc < 5) {
+			if (read_result != prev_read_result || i == 0) {
+				printf("0x%08X: 0x%0*lX\n", (int)(target + i*type_width), type_width * 2, read_result);
+				read_result_dupped = 0;
+			} else {
+				if (!read_result_dupped)
+					printf("...\n");
+				read_result_dupped = 1;
+			}
+		}
+
+		prev_read_result = read_result;
     }
 
     fflush(stdout);
 
-	if(argc > 4) {
+	if (argc > 4) {
 		writeval = strtoull(argv[4], NULL, 0);
 		switch(access_type) {
 			case 'b':
@@ -169,12 +176,17 @@ int main(int argc, char **argv) {
 				read_result = *((uint64_t *) virt_addr);
 				break;
 		}
-		printf("Written 0x%0*lX; readback 0x%*lX\n", type_width,
-		       writeval, type_width, read_result);
+		if (verbose)
+			printf("Written 0x%0*lX; readback 0x%*lX\n", type_width,
+				writeval, type_width, read_result);
+		else
+			printf("0x%08X: 0x%0*lX was 0x%0*lX\n", (int)(target),
+				type_width * 2, writeval,
+				type_width * 2, prev_read_result);
 		fflush(stdout);
 	}
 
-	if(munmap(map_base, map_size) == -1) PRINT_ERROR;
+	if (munmap(map_base, map_size) == -1) PRINT_ERROR;
     close(fd);
     return 0;
 }
